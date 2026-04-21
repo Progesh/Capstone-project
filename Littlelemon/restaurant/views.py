07308ab06d55fed0bf019cfe156d456a59114aa9
Progesh Subba <progesh.subba@laerdal.com>
2026-04-21 08:15:50 +0530

    added DRF and created serializers for menu api

--- Littlelemon/restaurant/views.py
+++ Littlelemon/restaurant/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Menu
+from .serializers import MenuSerializer
+
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html', {})
+    return render(request, "index.html", {})
+
+
+# Handles GET (list) and POST (create)
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+# Handles GET (single), PUT, DELETE
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
